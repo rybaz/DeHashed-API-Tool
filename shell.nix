b15d54e6405fb78063a4077788aa77475a27816a
rybaz <>
2024-10-10 17:01:09 -0400

    added flake and shell nix files

---
+++ shell.nix
@@ -0,0 +1,20 @@
+ { pkgs ? import <nixpkgs> {} }:
+
+ pkgs.mkShell {
+
+    packages = [ 
+	    # python
+    	pkgs.python312
+	    # python modules
+	    pkgs.python312Packages.beautifulsoup4
+	    pkgs.python312Packages.colorama
+	    pkgs.python312Packages.requests
+	  ];
+
+    shellHook = ''
+    	printf "===================================================================\n"
+    	printf "Now entering a devshell for DeHashed-API-Tool...n"
+    	printf "===================================================================\n"
+	'';
+}
+
