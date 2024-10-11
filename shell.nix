2cc64aca73a6a47803373ca86a87a6209abcf063
rybaz <>
2024-10-11 11:35:11 -0400

    removed unnecessary libs

--- shell.nix
+++ shell.nix
@@ -6,8 +6,6 @@
 	    # python
     	pkgs.python312
 	    # python modules
-	    pkgs.python312Packages.beautifulsoup4
-	    pkgs.python312Packages.colorama
 	    pkgs.python312Packages.requests
 	  ];
 
